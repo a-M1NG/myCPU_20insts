84df446d5f65928371b2299b027b3c5e9a0e88cc
a-M1NG <119879500+a-M1NG@users.noreply.github.com>
2024-12-25 02:22:41 +0800

    优化 regfile 模块，添加初始化逻辑并调整代码格式

--- regfile.v
+++ regfile.v
@@ -1,4 +1,4 @@
-module regfile(
+module regfile (
     input  wire        clk,
     // READ PORT 1
     input  wire [ 4:0] raddr1,
@@ -7,21 +7,27 @@ module regfile(
     input  wire [ 4:0] raddr2,
     output wire [31:0] rdata2,
     // WRITE PORT
-    input  wire        we,       //write enable, HIGH valid
+    input  wire        we,      //write enable, HIGH valid
     input  wire [ 4:0] waddr,
     input  wire [31:0] wdata
 );
-reg [31:0] rf[31:0];
+  reg [31:0] rf[31:0];
+  integer i;
+  initial begin
+    for (i = 0; i < 32; i = i + 1) begin
+      rf[i] = 32'b0;
+    end
+  end
 
-//WRITE
-always @(posedge clk) begin
+  //WRITE
+  always @(posedge clk) begin
     if (we) rf[waddr] <= wdata;
-end
+  end
 
-//READ OUT 1
-assign rdata1 = (raddr1==5'b0) ? 32'b0 : rf[raddr1];
+  //READ OUT 1
+  assign rdata1 = (raddr1 == 5'b0) ? 32'b0 : rf[raddr1];
 
-//READ OUT 2
-assign rdata2 = (raddr2==5'b0) ? 32'b0 : rf[raddr2];
+  //READ OUT 2
+  assign rdata2 = (raddr2 == 5'b0) ? 32'b0 : rf[raddr2];
 
 endmodule
